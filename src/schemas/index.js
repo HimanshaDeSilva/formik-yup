c9d476e25ec68a11ced5d82e08f769596eafe2a6
Himansha Dilshangi <desilvahima@gmail.com>
2025-02-17 19:41:09 +0530

    Initial commit without node_modules

---
+++ src/schemas/index.js
@@ -0,0 +1,17 @@
+import * as yup from "yup";
+
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
+
+export const basicSchema = yup.object().shape({
+  email: yup.string().email("Please enter a valid email").required("Required"),
+  age: yup.number().positive().integer().required("Required"),
+  password: yup
+    .string()
+    .min(5)
+    .matches(passwordRegex, { message: "Please create a stronger password" })
+    .required("Required"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Password must match")
+    .required("Required"),
+});
