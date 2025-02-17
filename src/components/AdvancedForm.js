20ea16daea69ef3d1f9d4972a31d5804cc6e982f
Himansha Dilshangi <desilvahima@gmail.com>
2025-02-17 20:48:52 +0530

    update custom components

--- src/components/AdvancedForm.js
+++ src/components/AdvancedForm.js
@@ -1,10 +1,45 @@
+import { Formik, Form, Field } from "formik";
+import CustomInput from "./CustomInput";
+import { advancedSchema } from "../schemas";
+import CustomSelect from "./CustomSelect";
+import CustomCheckbox from "./CustomCheckbox";
+
+const onSubmit = async (values, actions) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  actions.resetForm();
+};
 const AdvancedForm = () => {
-    return (
-      <form autoComplete="off">
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" placeholder="Enter your username" />
-      </form>
-    );
-  };
-  export default AdvancedForm;
-  
+  return (
+    <Formik
+      initialValues={{ username: "", jobType: "", acceptedTos: false }}
+      validationSchema={advancedSchema}
+      onSubmit={onSubmit}
+    >
+      {({isSubmitting}) => (
+        <Form>
+          {/* <Field type="text" name="name" placeholder="Name" /> */}
+          <CustomInput
+            label="Username"
+            name="username"
+            type="text"
+            placeholder="Enter your username"
+          />
+          <CustomSelect
+            label="Job Type"
+            name="jobType"
+            placeholder="Please select a job"
+          >
+            <option value="">Please select a job type</option>
+            <option value="developer">Developer</option>
+            <option value="designer">Designer</option>
+            <option value="manager">Product Manager</option>
+            <option value="other">Other</option>
+          </CustomSelect>
+          <CustomCheckbox type="checkbox" name="acceptedTos"></CustomCheckbox>
+          <button type="submit" disabled={isSubmitting}>Submit</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+export default AdvancedForm;
